a2066b76bb86567b0f799843cd02bdfcb1d7a1ab
Drago <dragodominguezfb@gmail.com>
2022-10-18 23:47:50 -0500

    Agregada la descarga de qr

--- src/routes/administador/generadorQR.js
+++ src/routes/administador/generadorQR.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const QRious = require("qrious");
+const QRCode = require("qrcode");
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -8,21 +8,18 @@ router.get("/", (req, res) => {
 
 //Metodo para generar un QR a partir de un texto, despues de generar el QR se envia como respuesta de descarga
 router.get("/:texto", (req, res) => {
-  //Obtiene el texto a partir de la url
   const texto = req.params.texto;
-  //Generar la imagen donde se guardara el QR
-  //Generar un canvas para poder generar el QR
-  const canvas = document.createElement("canvas");
-  //Generar el QR
-  const qr = new QRious({
-    element: canvas,
-    value: texto,
-    size: 500,
-    backgroundAlpha: 0, // 0 para fondo transparente
-    foreground: "#000", // Color del QR
-    level: "M", // Puede ser L,M,Q y H (L es el de menor nivel, H el mayor)
+  QRCode.toFileStream(res, texto, {
+    width: 300,
+    margin: 1,
+    color: {
+      dark: "#000000",
+      light: "#fff",
+    },
   });
-  //TODO Generar el enlace de descargar del QR
+  //despues de generar el QR se envia como respuesta de descarga
+  res.setHeader("Content-Type", "image/png");
+  res.setHeader("Content-Disposition", "attachment; filename=qr.png");
 });
 
 module.exports = router;
